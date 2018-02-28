f40b6d6530116e690ffe3e27e98c7cec7a4119ea
acistoldi <acistoldi@gmail.com>
2018-02-28 00:07:53 -0700

    index css and js

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+//pseudocode 
+var random_number;
+var win;
+var lose;
+
+
+//at start of game computer generates random number (19-120)
+random_number = Math.floor((Math.random()*120) + 19);
+    console.log(random_number);
+    
+//randomn generate generate crystal number (1-12) 
+for(var i=0; i<4; i++){
+    var crystal_num = Math.floor(Math.random()*12);
+    console.log(crysal_num);
+}
+
+//on click of crystal will add #points to player's total score hidden to user until after click
+<crystal1 onClick="totalFunction()">Click me</crystal1>
+//update the players score counter
+//user makes a number guess
+//link the crystal value to crystal buttons  in html
+//when user's total score = randomn computer number user wins and game restarts
+//when user's total score > randomn computer number user loses and game restarts
+//at start of new game, new randomn numbers for main number and crystals.
+//reset total score and score counter *do not reset wins/loses 
+
+//the value is stored and not displayed to user
